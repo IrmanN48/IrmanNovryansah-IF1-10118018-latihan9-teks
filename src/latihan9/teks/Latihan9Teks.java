c02dd2306a425891b44395f94dcbf030d8584095
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-09-25 22:51:49 +0700

    Add files via upload

---
+++ src/latihan9/teks/Latihan9Teks.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9.teks;
+
+/**
+ *
+ * @author Aero
+ * NAMA     : Irman Novryansah
+ * KELAS    : IF-1
+ * NIM      : 10118018
+ * Deskripsi Program : Program ini untuk menampilkan value teks
+ */
+public class Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        //Menambahkan nilai ke variabel
+        c = 'A';
+        
+        //Tampilan Output
+        System.out.println("Hasil" + newline + "char : " + tab + "berisi" + c);
+    }
+    
+}
